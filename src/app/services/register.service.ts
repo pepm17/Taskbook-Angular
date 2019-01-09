b6051ab60d4b3eb76d9cb239b7868e3cc5257408
Pablo Emilio <pablopupo1797@gmail.com>
2019-01-09 17:39:16 -0500

    FormsModule
    Importar el forms

--- src/app/services/register.service.ts
+++ src/app/services/register.service.ts
@@ -1,9 +1,11 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { RegisterModule } from '../models/register/register.module';
 
 @Injectable({
   providedIn: 'root'
 })
 export class RegisterService {
-
-  constructor() { }
+	selectedRegister
+	constructor(private http: HttpClient) { }
 }
