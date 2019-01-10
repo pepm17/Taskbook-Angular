4779050966f01ccd5b477eb990911703dc422cd1
Jorge Castro <mcand2297@gmail.com>
2019-01-09 23:04:44 -0500

    Correccion navbar
    Se incluyo el codigo que estaba en app.componet a navbar.component y se importo en el app.componet, se agrego el componente a app.module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms'
 import { AppRoutingModule, routingComponents } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    NavbarComponent,
     routingComponents
   ],
   imports: [
