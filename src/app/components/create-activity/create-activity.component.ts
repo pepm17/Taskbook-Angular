34bc68800a49cee471f16401bb3aaf3468890442
Pablo Emilio <pablopupo1797@gmail.com>
2019-01-15 02:05:23 -0500

    Correciones

--- src/app/components/create-activity/create-activity.component.ts
+++ src/app/components/create-activity/create-activity.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivityService } from '../../services/activity.service';
+import { NgForm } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-create-activity',
@@ -6,10 +9,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./create-activity.component.css']
 })
 export class CreateActivityComponent implements OnInit {
-
-  constructor() { }
+	public error: string;
+  constructor(private activityService: ActivityService, private router: Router) { }
 
   ngOnInit() {
   }
-
+  onSubmit(form: NgForm){
+	this.activityService.createActivity(form.value).subscribe(
+		res =>this.router.navigate(['/']),
+		err =>this.error = 'Something went wrong'
+	)}
 }
